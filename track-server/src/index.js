7845354321ddf544b4ca56805d7fbdea1e121d32
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-08 15:07:18 -0500

    Connect to mongodb

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,7 +1,23 @@
 // import express library
 const express = require('express');
+const mongoose= require('mongoose');
 
 const app = express();
+
+const mongoUri = 
+'mongodb+srv://admin:testpassword@cluster0.ayzpq.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo', err)
+})
 
 app.get('/', (req, res) => {
     res.send('Hi, there!')
