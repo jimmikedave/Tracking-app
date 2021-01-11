73d94c8636025b0c23887f55df9a6f4a7f37be90
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-11 12:37:19 -0500

    Require auth with jsonwebtoken

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose= require('mongoose');
 const bodyParser = require('body-parser'); //helper library that auto parses info of incoming req
 const authRoutes = require('./routes/authRoutes')
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
@@ -27,8 +28,8 @@ mongoose.connection.on('error', (err) => {
     console.error('Error connecting to mongo', err)
 })
 
-app.get('/', (req, res) => {
-    res.send('Hi, there!')
+app.get('/', requireAuth, (req, res) => {
+    res.send(`Your email: ${req.user.email}`)
 });
 
 app.listen(3000, () => {
--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // import mongoose model 
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken')
 const User = mongoose.model('User');
 
 const router = express.Router();
@@ -12,7 +13,9 @@ router.post('/signup', async (req, res) => {
         const user  = new User({ email, password });
         await user.save();
     
-        res.send('You made a post request')
+        //create token
+        const token = jwt.sign({userId: user._id}, 'MY_SECRET_KEY');
+        res.send({ token });
     } catch (err) {
         return res.status(422).send(err.message)
     }
