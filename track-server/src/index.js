7ac675e0712b8dba05c7a4a4fd5e358940c8e7c1
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2021-01-12 09:34:22 -0500

    Add location tracking schema

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,9 +1,11 @@
 require('./models/user');
+require('./models/Track');
 // import express library
 const express = require('express');
 const mongoose= require('mongoose');
 const bodyParser = require('body-parser'); //helper library that auto parses info of incoming req
 const authRoutes = require('./routes/authRoutes')
+const trackRoutes = require('./routes/trackRoutes')
 const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
@@ -12,6 +14,7 @@ const app = express();
 //then sends the parsed info to the req param
 app.use(bodyParser.json()); 
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri = 
     'mongodb+srv://admin:testpassword@cluster0.ayzpq.mongodb.net/test?retryWrites=true&w=majority'
